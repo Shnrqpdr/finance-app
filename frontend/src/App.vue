d52b65c4210b587104efd5385968d6e8afeca407
Shnrqpdr <hnrqpdr.sc@gmail.com>
2022-09-03 21:30:53 -0300

    Ajusta navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,5 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar app flat>
-      <v-avatar
-        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
-        size="32"
-      ></v-avatar>
-
-      <v-tabs centered class="ml-n9" color="grey darken-1">
-        <v-tab v-for="link in links" :key="link">
-          {{ link }}
-        </v-tab>
-      </v-tabs>
-
-      <v-icon
-        large
-        class="dark-mode"
-        @click="$vuetify.theme.dark = !$vuetify.theme.dark"
-        >mdi-theme-light-dark</v-icon
-      >
-    </v-app-bar>
-
     <v-main class="background">
       <v-container style="height: 100%">
         <router-view />
@@ -31,10 +11,6 @@
 <script>
 export default {
   name: "App",
-
-  data: () => ({
-    links: ["Login", "About", "Github"],
-  }),
 };
 </script>
 <style lang="scss" scoped>
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,11 +1,32 @@
 <template>
-  <v-row class="row-card-login">
-    <v-col cols="12" sm="6">
-      <v-sheet height="50vh" rounded="lg">
-        <login-page />
-      </v-sheet>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-app-bar app flat>
+      <v-avatar
+        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
+        size="32"
+      ></v-avatar>
+
+      <v-tabs centered class="ml-n9" color="grey darken-1">
+        <v-tab v-for="link in links" :key="link">
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+
+      <v-icon
+        large
+        class="dark-mode"
+        @click="$vuetify.theme.dark = !$vuetify.theme.dark"
+        >mdi-theme-light-dark</v-icon
+      >
+    </v-app-bar>
+    <v-row class="row-card-login">
+      <v-col cols="12" sm="6">
+        <v-sheet height="50vh" rounded="lg">
+          <login-page />
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -14,7 +35,9 @@ import LoginPage from "../components/login/LoginPage";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Login",
-
+  data: () => ({
+    links: ["Login", "About", "Github"],
+  }),
   components: {
     LoginPage,
   },
