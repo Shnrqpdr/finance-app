47d1f0cc5eac0738173e6fb5abdbd812e0067fb8
Shnrqpdr <hnrqpdr.sc@gmail.com>
2022-09-04 13:27:15 -0300

    Sidebar

--- frontend/src/components/home/HomePage.vue
+++ frontend/src/components/home/HomePage.vue
@@ -1,22 +1,27 @@
 <template>
   <v-container>
     <v-app-bar app flat>
-      <v-button>
+      <v-btn depressed>
         <v-icon
           large
           class="dark-mode"
           @click="$vuetify.theme.dark = !$vuetify.theme.dark"
           >mdi-theme-light-dark</v-icon
         >
-      </v-button>
+      </v-btn>
       <v-spacer />
-      <v-button>
+      <v-btn depressed>
         <v-icon large @click="sair()">mdi-exit-to-app</v-icon>
-      </v-button>
+      </v-btn>
     </v-app-bar>
+
     <v-row class="text-center">
-      <v-col cols="3"> <v-sheet height="50vh" rounded="lg"></v-sheet> </v-col>
-      <v-col cols="9" class="mb-4">
+      <v-col cols="4">
+        <v-navigation-drawer permanent>
+          <profile />
+        </v-navigation-drawer>
+      </v-col>
+      <v-col cols="8" class="mb-4">
         <v-sheet height="50vh" rounded="lg"></v-sheet>
       </v-col>
     </v-row>
@@ -24,8 +29,13 @@
 </template>
 
 <script>
+import Profile from "./Profile";
+
 export default {
   name: "HomePage",
+  components: {
+    Profile,
+  },
 
   data: () => ({}),
   methods: {
